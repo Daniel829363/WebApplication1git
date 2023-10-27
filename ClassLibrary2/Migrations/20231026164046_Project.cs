ea72b8a68660a51ebc6304720e1682b442154274
daniel <danielsydykov12@gmail.com>
2023-10-27 20:28:27 +0600

    ++

---
+++ ClassLibrary2/Migrations/20231026164046_Project.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class Project : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "EmployeeId1",
+                table: "Employees",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_EmployeeId1",
+                table: "Employees",
+                column: "EmployeeId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_Employees_EmployeeId1",
+                table: "Employees",
+                column: "EmployeeId1",
+                principalTable: "Employees",
+                principalColumn: "EmployeeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Employees_EmployeeId1",
+                table: "Employees");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Employees_EmployeeId1",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "EmployeeId1",
+                table: "Employees");
+        }
+    }
+}
